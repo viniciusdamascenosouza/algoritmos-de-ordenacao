9804be83af7fd60354ad1c02f0a1df3af2fe28e0
viniciusdamascenosouza <viniciusdamascenosouza@gmail.com>
2023-02-10 08:34:53 -0300

    Comparando livros mais baratos

---
+++ index.js
@@ -0,0 +1,11 @@
+const precoLivros = [20, 15, 30, 50, 45, 20];
+
+let maisBarato = 0;
+
+for (let atual = 0; atual < precoLivros.length; atual++) {
+  if (precoLivros[atual] < precoLivros[maisBarato]) {
+    maisBarato = atual;
+  }
+}
+
+console.log(`O livro mais barato custa ${precoLivros[maisBarato]}`);
