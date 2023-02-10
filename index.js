9ea98f3ece7d3587b0f37595b4880ee12f4e86e2
viniciusdamascenosouza <viniciusdamascenosouza@gmail.com>
2023-02-10 16:57:22 -0300

    correções

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-const precoLivros = [20, 15, 30, 50, 45, 20];
+const livros = require('./listaLivros');
 
 let maisBarato = 0;
 
-for (let atual = 0; atual < precoLivros.length; atual++) {
-  if (precoLivros[atual] < precoLivros[maisBarato]) {
-    maisBarato = atual;
+for (let atual = 0; atual < livros.length; atual++) {
+  if (livros[atual].preco < livros[maisBarato].preco) {
+    maisBarato = atual
   }
 }
 
-console.log(`O livro mais barato custa ${precoLivros[maisBarato]} reais.`);
+console.log(`o livro mais barato custa ${livros[maisBarato].preco} reais, e o título é ${livros[maisBarato].titulo}.`)
