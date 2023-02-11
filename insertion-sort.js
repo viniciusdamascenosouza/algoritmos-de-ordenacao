45c25864294c03bfedd0ee9f0fd042dd1571f1b0
viniciusdamascenosouza <viniciusdamascenosouza@gmail.com>
2023-02-11 09:51:31 -0300

    Programando o algoritmo

---
+++ insertion-sort.js
@@ -0,0 +1,21 @@
+const livros = require('./listaLivros');
+
+function insertionSort(arr) {
+
+    for (let atual = 0; atual < livros.length; atual++) {
+        let analise = atual;
+        while (analise > 0 && arr[analise].preco < arr[analise - 1].preco) {
+            let itemAnalise = arr[analise];
+            let itemAnterior = arr[analise - 1];
+
+            arr[analise] = itemAnterior;
+            arr[analise - 1] = itemAnalise;
+
+            analise--;
+        }
+    }
+
+    console.log(arr);
+}
+
+insertionSort(livros);
--- selection-sort.js
+++ selection-sort.js
@@ -5,12 +5,9 @@ for (let atual = 0; atual < livros.length - 1; atual++) {
     let menor = menorValor(livros, atual);
 
     let livroAtual = livros[atual];
-    console.log('Posição atual', atual)
-    console.log('Livro atual', livros[atual]);
 
     let livroMenorPreco = livros[menor];
-    console.log('Livro menor preço', livros[menor])
-
+    
     livros[atual] = livroMenorPreco;
     livros[menor] = livroAtual;
 }
