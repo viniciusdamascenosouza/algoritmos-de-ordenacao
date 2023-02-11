56180ed92475f99a258bd7e33aafc8da7aa84302
viniciusdamascenosouza <viniciusdamascenosouza@gmail.com>
2023-02-11 09:28:13 -0300

    insertion-sort

--- menorValor.js
+++ menorValor.js
@@ -1,10 +1,9 @@
 const livros = require('./listaLivros');
 
 function menorValor(arrProdutos, posicaoInicial) {
-
   let maisBarato = posicaoInicial;
 
-  for (let atual = 0; atual < arrProdutos.length; atual++) {
+  for (let atual = posicaoInicial; atual < arrProdutos.length; atual++) {
     if (arrProdutos[atual].preco < arrProdutos[maisBarato].preco) {
       maisBarato = atual
     }
